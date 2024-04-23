a5e6ecba021988295771a155bf1a55a6817e31dc
Maya Chavoshi <maya.chavoshi@rystadenergy.com>
2024-04-23 16:32:31 +0300

    add setup file

--- start.py
+++ start.py
@@ -1,2 +1,8 @@
 def say_hello():
     print("Hello, world!")
+
+def install_package():
+    print("Installing package...")
+    import subprocess
+    # pip install git+https://github.com/your_username/your_project.git
+    subprocess.run(["pip", "install", "git+https://github.com/MChavoshi96/test.git"])
